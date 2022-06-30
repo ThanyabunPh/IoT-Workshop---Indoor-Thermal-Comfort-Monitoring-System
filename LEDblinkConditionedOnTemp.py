ac41f5cac268e980d1b8335040a6748142a60cab
Mallika Kliangkhlao <42213474+mlkklkhl@users.noreply.github.com>
2022-06-30 20:03:32 +0700

    Create LEDblinkConditionedOnTemp.py

---
+++ LEDblinkConditionedOnTemp.py
@@ -0,0 +1,43 @@
+import esp
+import sys
+import utime as time
+import machine
+import dht
+
+rtc = machine.RTC()
+rtc.datetime((2022, 6, 30, 5, 14, 58, 0, 362)) 
+# set a specific date and time 
+
+# Create a dht object that refers to the sensor data pin, in this case is PIN 15
+SENSOR_PIN = 15 
+sensor = dht.DHT22(machine.Pin(SENSOR_PIN))
+
+LEDGreen = machine.Pin(32, machine.Pin.OUT)
+LEDRed = machine.Pin(33, machine.Pin.OUT)
+
+for i in range(10):
+  
+  try:
+      LEDGreen.value(0)
+      LEDRed.value(0)
+      
+      sensor.measure()
+      temp = sensor.temperature()
+      hum = sensor.humidity()
+      
+      t = rtc.datetime()
+      
+      now = '{:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}'.format(t[0], t[1], t[2], t[4], t[5], t[6])
+      
+      if temp < 23.0 or temp > 27.0: LEDRed.value(1)
+      else: LEDGreen.value(1)
+      
+      print("Time: ",  now)
+      print('Sensor Temperature: %3.1f C' %temp)
+      print('Sensor Humidity: %3.1f %%' %hum)
+      print('  ')
+      
+      time.sleep(5)
+      
+  except OSError as e:
+    print('Failed to read data from the DHT22 sensor.')
