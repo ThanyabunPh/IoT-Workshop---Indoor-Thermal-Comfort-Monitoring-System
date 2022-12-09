18e3b2a244a932cae01c903e847d4e12d2efe469
Mallika Kliangkhlao <42213474+mlkklkhl@users.noreply.github.com>
2022-12-09 11:29:16 +0700

    Update Blink.py

--- Blink.py
+++ Blink.py
@@ -4,22 +4,15 @@ import utime as time
 import machine
 import dht
 
-LEDGreen = machine.Pin(32, machine.Pin.OUT)
-LEDRed = machine.Pin(33, machine.Pin.OUT)
+LEDPin = machine.Pin(32, machine.Pin.OUT)
 
 for i in range(10):
   
   try:
-      LEDGreen.value(0)
-      LEDRed.value(0)
-      
+      LEDPin.value(0)
       time.sleep(1)
-      LEDRed.value(1)
       
-      time.sleep(1)
-      LEDGreen.value(1)
-      LEDRed.value(0)
-      
+      LEDPin.value(1)
       time.sleep(1)
       
   except OSError as e:
